83cd26277acf562c47c02e45bd218a5f4c4b5c61
Brian Yangas <yangasb@gmail.com>
2016-09-12 19:13:34 -0700

    moving js filesinto seperate file

---
+++ JobMonDashboard/app/app.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module('main', [])
+
+(function () {
+
+    app.controller('jobsController', ['$scope', '$http', function JobsController($scope, $http) {
+
+        getJobs();
+
+
+       var getJobs = function getJobs() {
+
+            $http.get('/api/jobs/').then(function success(response) {
+
+                for (var i = 0; i < response.data.length; i++) {
+
+                    //temp stub until we get the instances.
+                    response.data[i].instanceCount = 0;
+                }
+                $scope.jobList = response.data;
+
+                console.log($scope.jobList);
+                getInstances();
+            },
+            function error(response) {
+                alert(response.data);
+            });
+        }
+
+       var getInstances = function getInstances() {
+            $http.get('/api/instances/').then(function success(response) {
+                $scope.jobInstances = response.data;
+                console.log("got instances");
+                console.log(response.data);
+            },
+            function error(response) {
+                alert(response.data);
+            });
+        }
+
+    }]);
+})()
