56ff4bd827ade659e72257208149a17587f884a8
Brian Yangas <yangasb@gmail.com>
2016-09-14 12:06:33 -0700

    Having fun with Karma and Jasmine unit tests.

---
+++ JobMonDashboard/app/util.js
@@ -0,0 +1,56 @@
+﻿"use strict";
+var JMUtil = JMUtil || {};//namespace creation
+
+JMUtil.isArray = function isArray(arr) {
+    if (!arr)
+        return false;
+
+    var hasLength = arr.hasOwnProperty("length");
+    var isString = typeof arr === 'string';
+
+    if (isString || !hasLength) {
+        return false; //empty array, because source is not array
+    }
+    return true;
+}
+
+JMUtil.flattenNode = function flattenNode(sourceArray, propertyName) {
+
+    var result = [];
+
+    if (JMUtil.isArray(sourceArray) === false) {
+        return result; //empty array, because source is not array
+    }
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        var t = sourceArray[i][propertyName];
+        if (t) {
+            for (var j = 0; j < t.length; j++) {
+                result.push(t[j]);
+            }
+        }
+    }
+    return result;
+}
+
+JMUtil.countUnique = function countUnique(source) {
+    var result = {};
+
+    if (JMUtil.isArray(source) === false) {
+        return result;
+    }
+
+
+    for (var i = 0; i < source.length; i++) {
+        var item = source[i];
+
+        if (!result.hasOwnProperty(item)) {
+            result[item] = 1;
+        }
+        else {
+            result[item] += 1;
+        }
+    }
+    console.log(result);
+    return result;
+}
---
+++ JobMonDashboard/test/test_util.js
@@ -0,0 +1,63 @@
+﻿describe("test utility functions", function () {
+    //beforeEach(function () {
+    //    console.log("beofre");
+    //});
+    describe("utility to pull data from an object array that has array properties", function () {
+        it("should flatten object to array", function () {
+            expect(JMUtil).not.toBeNull();//make sure the object got imported
+
+            var testData = [
+                { testProperty: ["Thing1", "Thing2"] },
+                { testProperty: ["Thing3"] },
+                { notTestProperty: ["dummy"]}
+            ];
+
+            var flattened = JMUtil.flattenNode(testData, 'testProperty');
+            expect(flattened).not.toBeNull();
+            expect(flattened.length).toBe(3);
+        })
+
+        it("should work for nulls", function () {
+            var flattened = JMUtil.flattenNode(null, 'testProperty');
+            expect(flattened).not.toBeNull();
+            expect(flattened.length).toBe(0);
+        });
+
+        it("should work for empty array", function () {
+            var flattened = JMUtil.flattenNode([], 'testProperty');
+            expect(flattened).not.toBeNull();
+            expect(flattened.length).toBe(0);
+        });
+
+        it("should NOT work for string", function () {
+            var flattened = JMUtil.flattenNode('test string', 'testProperty');
+            expect(flattened).not.toBeNull();
+            expect(flattened.length).toBe(0);
+        });
+
+        it("should NOT work for object", function () {
+            var flattened = JMUtil.flattenNode({}, 'testProperty');
+            expect(flattened).not.toBeNull();
+            expect(flattened.length).toBe(0);
+        });
+
+    });
+
+    describe("Tests for counting unique instances in array", function UniqueItemsTest() {
+        it("should count numbers", function countNumbers() {
+            var bob = JMUtil.countUnique([1, 2, 2]);
+            expect(bob).not.toBeNull();
+            var hasCorrectProperty = bob.hasOwnProperty(2);
+            expect(hasCorrectProperty).toBe(true);
+            expect(bob['2']).toBe(2);
+        });
+
+        it("should count strings", function countNumbers() {
+            var bob = JMUtil.countUnique(["one", "two", "two"]);
+            expect(bob).not.toBeNull();
+            var hasCorrectProperty = bob.hasOwnProperty("two");
+            expect(hasCorrectProperty).toBe(true);
+            expect(bob.two).toBe(2);
+        });
+    });
+})
